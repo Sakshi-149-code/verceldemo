64c8afa45089b11d69744096302461f216f7897d
Sakshi-149-code <mohitesakshi168@gmail.com>
2025-10-27 12:22:12 +0530

    Vercel Demo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  
+
+  function dothis()
+  {
+    window.alert("Hello");
+  }
+  return (
+    <>
+    <h1>Hii there,I am an Engineer.</h1>
+    <button onClick={dothis}>Click me!</button>
+      </>
+  )
+}
+
+export default App
